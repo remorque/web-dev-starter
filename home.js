2d0ae6d4af2001ab29b3d6e07e4a57a2109f202b
remorque <remorque@protonmail.com>
2021-01-25 09:46:23 +0100

    Program flows

--- home.js
+++ home.js
@@ -1,21 +1,57 @@
-let price = 49.99,
-    taxRate = 0.07,
-    name = 'Hiking Boots',
-    discounted = false;
+// let price = 49.99,
+//     taxRate = 0.07,
+//     name = 'Hiking Boots',
+//     discounted = false;
 
-const fixedPrice = 10;
+// const fixedPrice = 10;
 
-price += fixedPrice;
+// price += fixedPrice;
 
-let message = `Hello
+// let message = `Hello
 
 
-World`
-console.log(message)
+// World`
+// console.log(message)
 
-let person = {
-    firstName: 'John',
-    lastName: 'Adams'
+// let person = {
+//     firstName: 'John',
+//     lastName: 'Adams'
+// }
+
+// showMessage(person.firstName);
+
+let price = 20;
+
+if ( +(1.1 + 1.3).toFixed(2) !== 2.4 ) {
+    showMessage('true');
 }
 
-showMessage(person.firstName);
+if (price > 100) {
+    console.log('huge')
+}
+else if (price > 50) {
+    console.log('big')
+}
+else {
+    console.log('small')
+}
+
+// Ternary operator
+(price > 10) ? showMessage('expensive') : showMessage('cheap')
+
+// Loops
+for (let i=0; i <= 5; i++) {
+    console.log(i)
+}
+
+let count = 1;
+while (count < 5) {
+    console.log(count);
+    count++;
+}
+
+// Always executed once
+do {
+    console.log(count);
+    count++;
+} while (count < 5)
